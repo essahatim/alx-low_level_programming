e8bd0583fd875bf56df1b3651560124c4d03bfe3
essahatim <hatimessabagh@gmail.com>
2023-06-15 14:55:14 -0700

    fixed error

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,12 @@
 #!/bin/bash
 #include<stdio.h>
- int main (void)
+/**
+ * main - Entry point
+ *
+ * return: Always 0 (Success)
+ */
+int main (void)
 {
-      puts :("Programming is like building a multilingual puzzle\n");
-      return (0);
-}		    
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}	
