5bbdaa47b642413cd719ba0bb22a9e8005f913a9
essahatim <hatimessabagh@gmail.com>
2023-06-15 17:48:31 -0700

    fix error: invalid preprocessing directive #!

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,8 @@
-#!/bin/bash
 #include <stdio.h>
 
 /**
  * main - Prints "Programming is like building a multilingual
- *                puzzle, followed by a new line.
+ *                puzzle, followed by a new line."
  *
  * Return: Always 0.
  */
