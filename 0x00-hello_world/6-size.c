7ee4bc625c0616647e08ef5fa6df00185669aa28
essahatim <hatimessabagh@gmail.com>
2023-06-15 19:06:21 -0700

    fixed error

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+
 /**
  * main - prints the size of various types on 
  * the computer it is compiled and run on
@@ -7,10 +8,10 @@
 int main (void)
 
 {
-        printf("size of a char: %zu byte (s)\n", sizeof(char));
-        printf("size of an int: %zu byte (s)\n", sizeof(int));
-        printf("size of a long int: %zu byte (s)\n", sizeof(long int));
-        printf("size of a long long int: %zu byte (s)\n", sizeof (long long int));
-        printf("size of a float: %zu byte (s)\n", sizeof (float));
+        printf("size of a char: %zu byte(s)\n", sizeof(char));
+        printf("size of an int: %zu byte(s)\n", sizeof(int));
+        printf("size of a long int: %zu byte(s)\n", sizeof(long int));
+        printf("size of a long long int: %zu byte(s)\n", sizeof(long long int));
+        printf("size of a float: %zu byte(s)\n", sizeof(float));
         return (0);
 }
