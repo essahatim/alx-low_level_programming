4698eb4a15a22cc669021cad2697898b94d05f3c
essahatim <hatimessabagh@gmail.com>
2023-06-19 19:08:58 -0700

    update the code

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -9,9 +9,9 @@
 int main(void)
 {
         int d;
-
-        for (d = '0'; d <= '9'; d++)
-        putchar(d);
-        putchar('\n');
-        return (0);
+		
+		for (d = '0'; d <= '9'; d++)
+        	putchar(d);
+        	putchar('\n');
+        	return (0);
 }
