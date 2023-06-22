d35c49b504a5b4b07877f956b4006e32825aecfa
essahatim <hatimessabagh@gmail.com>
2023-06-21 18:01:26 -0700

    add 101-natural.c

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,21 @@
+#include"main.h"
+/**
+ * main - Prints the sum of all multipes of 3 or 5 up to 1024
+ *
+ * Return: Always (success)
+ */
+int main(void)
+{
+	int i, j = 0;
+
+	while (i < 1024)
+	{
+	if ((i % 3 == 0) || (i % 5 == 0))
+	{
+	j += i;
+	}
+	i++
+	}
+	printf("%d\n", j);
+	return (0);
+}
