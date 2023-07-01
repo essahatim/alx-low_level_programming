c0795310c7816eaf9140711d6643e4aa63821ef6
essahatim <hatimessabagh@gmail.com>
2023-06-30 20:10:27 -0700

    fixed error

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,10 +1,10 @@
 #include"main.h"
 /**
- * reverse_array - reverses the content of an array of integers.
+ * reverse_array - Reverses the content of an array of integers.
  * @a: array
  * @n: number element of array
  * Return: void
- */*
+ */
 void reverse_array(int *a, int n)
 {
 	int i;
