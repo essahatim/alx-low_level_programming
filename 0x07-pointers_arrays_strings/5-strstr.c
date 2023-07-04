89fed642032aa0044305019bbedc6385d4582fbe
essahatim <hatimessabagh@gmail.com>
2023-07-03 19:09:59 -0700

    add changes

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -9,16 +9,15 @@ char *_strstr(char *haystack, char *needle)
 {
 	for (; *haystack != '\0'; haystack++)
 	{
-		char *l = haystack;
-		char *p = needle;
+	char *l = haystack;
+	char *p = needle;
 
-		while (*l == *p && *p != '\0')
-		{
-		l++;
-		p++;
-		}
-		if (*p == '\0')
-		return (haystack);
+	while (*l == *p && *p != '\0')
+	{
+	l++;
+	p++;
 	}
-	return (0);
-}	
+	if (*p == '\0')
+	return (haystack);
+	}
+}
