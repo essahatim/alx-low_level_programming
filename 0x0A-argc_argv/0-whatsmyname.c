8e1258cd65ff24aabe27b09c19dd7d297976a9a9
essahatim <hatimessabagh@gmail.com>
2023-07-12 16:44:34 -0700

    add 0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include"main.h"
+#include<stdio.h>
+/**
+ * main - prints its name, followed by a new line.
+ * @argc: number of arguments
+ * @rgv: array of arguments
+ * Return: Always 0 (Success)
+ */
+int main(int argc _attribute__((unused)). char *argv[])
+{
+	printf("%s\n", *argv);
+	return (0);
+}
