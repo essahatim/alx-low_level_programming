f0f467c17325ea83a50b4294942b1b46765ffa90
essahatim <hatimessabagh@gmail.com>
2023-07-27 17:50:19 -0700

    another fixed

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -2,22 +2,21 @@
 #include "lists.h"
 /**
  * print_list - Prints all the elements of a list_t list .
- * @h: Pointer to the list_t
- * Return: The number of nodes
+ * @h: The pointer to the list_t
+ * Return: The num of nodes
  */
 size_t print_list(const list_t *h)
 {
-	size_t p = 0;
+	size_t a = 0;
 
 	while (h)
 	{
-		if (!h->str)
-			printf("[0] (nil)\n");
-		else
-			printf("[%u] %p\n", h->len, h->str);
-		h = h->next;
-		p++;
+	if (!h->str)
+	printf("[0] (nil)\n");
+	else
+		printf("[%u] %a\n", h->len, h->str);
+	h = h->next;
+	a++;
 	}
-
-	return (p);
+	return (a);
 }
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,15 +1,13 @@
 #ifndef LISTS_H
 #define LISTS_H
-
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
  * @len: length of the string
  * @next: points to the next node
- *
- * Description: singly linked list node structure
- * for Holberton project
+ * Description: Singly linked list node structure
  */
+
 typedef struct list_s
 {
 	char *str;
@@ -18,8 +16,8 @@ typedef struct list_s
 } list_t;
 
 size_t print_list(const list_t *h);
-void free_list(list_t *head);
 size_t list_len(const list_t *h);
+void free_list(list_t *head);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 
