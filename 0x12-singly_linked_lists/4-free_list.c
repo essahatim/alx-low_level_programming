1707169bf58cc57f46f4b1042dae9410f4cae3a6
essahatim <hatimessabagh@gmail.com>
2023-07-27 18:24:36 -0700

    fixed error

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,18 +1,18 @@
 #include <stdlib.h>
 #include "lists.h"
 /**
- * free_list - frees a linked list
- * @head: list_t list to be freed
+ * free_list - Frees a list_t list.
+ * @head: The list_t list to free
  */
 void free_list(list_t *head)
 {
-	list_t *temp;
+	list_t *tep;
 
 	while (head)
 	{
-		temp = head->next;
-		free(head->str);
-		free(head);
-		head = temp;
+	tep = head->next;
+	free(head->str);
+	free(head);
+	head = tep;
 	}
 }
