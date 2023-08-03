b0c529e4c0f7bb5389cad47a4d79343360df0901
essahatim <hatimessabagh@gmail.com>
2023-08-03 13:05:56 -0700

    fixed error

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -8,7 +8,7 @@ void print_binary(unsigned long int n)
 	int a, count = 0;
 	unsigned long int current;
 
-	for (i = 63; a >= 0; a--)
+	for (a = 63; a >= 0; a--)
 	{
 		current = n >> a;
 		if (current & 1)
