94a31e1aac654627e4fbd5b25d52f2de4f477a03
assahatim <hatimessabagh@gmail.com>
2023-09-04 16:54:59 -0700

    fixed error

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -8,16 +8,16 @@
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
-	ssize_t t, w;
+	ssize_t bytes, w;
 	char *buf;
 
-	if (!filename || !letters)
-		return (0);
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
-	t = read(fd, buf, letters);
-	w = write(STDOUT_FILENO, buf, t);
+	buf = malloc(sizeof(char) * letters);
+	bytes = read(fd, buf, letters);
+	w = write(STDOUT_FILENO, buf, bytes);
+	free(buf);
 	close(fd);
 	return (w);
 }
