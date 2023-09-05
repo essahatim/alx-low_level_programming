4255f2aa9a36233638a72a3af95c74b4f3dc0944
assahatim <hatimessabagh@gmail.com>
2023-09-04 19:59:33 -0700

    fixed error

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -6,12 +6,15 @@
  * If file_to cannot be created or written to - exit code 99.
  * If file_to or file_from_fd cannot be closed - exit code 100.
  */
+char *create_buf(char *file);
+void close_file(int fd);
+
 /**
  * create_buf - Allocates 1024 bytes for a buf.
  * @file: The name of the file buf is storing chars for.
  * Return: The newly-allocated buff.
  */
-char *create_buf(char *file)
+char *create_buffer(char *file)
 {
 	char *buf;
 
@@ -24,10 +27,25 @@ char *create_buf(char *file)
 	return (buf);
 }
 /**
+ * close_file - Closes file.
+ * @fd: The file to be closed.
+ */
+void close_file(int fd)
+{
+	int cl;
+
+	cl = close(fd);
+	if (cl == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+		exit(100);
+	}
+}
+/**
  * main - Copies the contents of a file to another file.
  * @ac: The num of arguments.
  * @av: An array of pointers to the arguments.
- * Return: 0 (success).
+ * Return: 0 on success.
  */
 int main(int ac, char *av[])
 {
@@ -61,21 +79,6 @@ int main(int ac, char *av[])
 		r = read(from_fd, buffer, 1024);
 		to_fd = open(av[2], O_WRONLY | O_APPEND);
 	} while (r > 0);
-/**
- * close_file - Closes file.
- * @fd: The file to be closed.
- */
-void close_file(int fd)
-{
-	int cl;
-
-	cl = close(fd);
-	if (cl == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
-		exit(100);
-	}
-}
 	free(buf);
 	close_file(from_fd);
 	close_file(to_fd);
