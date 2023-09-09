c7eff0baf8ac9bc5dff6e39eb375e8798e54b78d
assahatim <hatimessabagh@gmail.com>
2023-09-08 19:43:29 -0700

    add changes

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,20 +1,15 @@
 #include "main.h"
-
+char *create_buf(char *file);
 /**
+ * create_buf - Allocates 1024 bytes for a buf.
+ * @file: The name of the file.
+ * Return: The newly-allocated buf.
+ *
  * Description:
  * If the argument count is incorrect - exit code 97.
  * If file_from does not exist or cannot be read - exit code 98.
  * If file_to cannot be created or written to - exit code 99.
  * If file_to or file_from cannot be closed - exit code 100.
- */
-
-char *create_buf(char *file);
-void close_file(int fd);
-
- /**
- * create_buf - Allocates 1024 bytes for a buf.
- * @file: The name of the file.
- * Return: The newly-allocated buf.
  */
 char *create_buf(char *file)
 {
@@ -30,7 +25,7 @@ char *create_buf(char *file)
 	return (buf);
 }
 
-/**
+ /**
  * main - Copies the contents of a file to another file.
  * @ac: The num of arguments.
  * @av: An array of pointers to the arguments.
@@ -40,35 +35,35 @@ int main(int ac, char *av[])
 {
 	int from_fd = 0, to_fd = 0;
 	ssize_t r, w;
-	char *buf;
+	char *buffer;
 
 	if (ac != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from_fd file_to_fd\n");
 		exit(97);
 	}
-	buf = create_buf(av[2]);
+	buffer = create_buffer(av[2]);
 	from_fd = open(av[1], O_RDONLY);
-	r = read(from_fd, buf, 1024);
+	r = read(from_fd, buffer, 1024);
 	to_fd = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	do {
 		if (from_fd == -1 || r == -1)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't read from_fd file %s\n", av[1]);
-			free(buf);
+			free(buffer);
 			exit(98);
 		}
-		w = write(to_fd, buf, r);
+		w = write(to_fd, buffer, r);
 		if (to_fd == -1 || w == -1)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't write to_fd %s\n", av[2]);
-			free(buf);
+			free(buffer);
 			exit(99);
 		}
-		r = read(from_fd, buf, 1024);
+		r = read(from_fd, buffer, 1024);
 		to_fd = open(av[2], O_WRONLY | O_APPEND);
 	} while (r > 0);
-	free(buf);
+	free(buffer);
 	close_file(from_fd);
 	close_file(to_fd);
 	return (0);
