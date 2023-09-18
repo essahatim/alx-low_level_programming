837f96134cfd054830e289d8bad1a6218f187f48
assahatim <hatimessabagh@gmail.com>
2023-09-18 15:39:12 -0700

    add changes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,16 +1,15 @@
 #include<stdio.h>
 /**
- * main - entry points
+ * main - Entry points
  *
- * return:Always 0.
+ * Return: Always 0.
  */
 int main(void)
-}
-	printf("size of a char: %d bety(s)\n"), sizeof(char));
-	printf("size of an int: %d bety(s)\n"), sizeof(int));
-	printf("size of a long int: %d baty(s)\n"), sizeof(long int));
-	printf("size of a long long int: %d bety(s)\n"), sizeof(long long int));
-	printf("size of a float: %d bety(s)\n"), sizeof(float));
+{
+	printf("size of a char: %d bety(s)\n", sizeof(char));
+	printf("size of an int: %d bety(s)\n", sizeof(int));
+	printf("size of a long int: %d baty(s)\n", sizeof(long int));
+	printf("size of a long long int: %d bety(s)\n", sizeof(long long int));
+	printf("size of a float: %d bety(s)\n", sizeof(float));
 	return (0);
 }
-
