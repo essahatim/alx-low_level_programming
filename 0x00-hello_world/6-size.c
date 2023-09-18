fe157102871be087b1419cdb1c900d03a396efa0
assahatim <hatimessabagh@gmail.com>
2023-09-18 15:47:10 -0700

    add changes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,10 +6,10 @@
  */
 int main(void)
 {
-	printf("size of a char: %d bety(s)\n", sizeof(char));
-	printf("size of an int: %d bety(s)\n", sizeof(int));
-	printf("size of a long int: %d baty(s)\n", sizeof(long int));
-	printf("size of a long long int: %d bety(s)\n", sizeof(long long int));
-	printf("size of a float: %d bety(s)\n", sizeof(float));
+	printf("size of a char: %ld bety(s)\n", sizeof(char));
+	printf("size of an int: %ld bety(s)\n", sizeof(int));
+	printf("size of a long int: %ld baty(s)\n", sizeof(long int));
+	printf("size of a long long int: %ld bety(s)\n", sizeof(long long int));
+	printf("size of a float: %ld bety(s)\n", sizeof(float));
 	return (0);
 }
