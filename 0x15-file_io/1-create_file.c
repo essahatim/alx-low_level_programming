5a42e4ab83c72b707e30f1a7d25c1f530843e878
essahatim <hatimessabagh@gmail.com>
2023-10-30 13:47:13 -0700

    add 1-create_file.c

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * create_file - Creates a file.
+ * @filename: The file created.
+ * @text_content: The text writed.
+ *
+ * Return: 1 (success) or 0 (failure).
+ */
+int create_file(const char *filename, char *text_content)
+{
+	ssize_t fd, w = 0, len = 0;
+
+	if (!filename)
+		return (-1);
+	if (text_content != NULL)
+		for (;text_content[len];len++)
+	fd = open(filename, O_WRONLY | O_CRAET | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
+	if (len)
+		w = write(fd, text_content, len)
+	close (fd);
+	return (w == len ? 1 : -1)
+}
