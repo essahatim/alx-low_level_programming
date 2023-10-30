ad3a2fce506a71dc9fb954fbe10928b10b20b902
essahatim <hatimessabagh@gmail.com>
2023-10-30 14:37:30 -0700

    add changes

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -6,9 +6,9 @@
  *
  * Return: 1 (success) or 0 (failure).
  */
-int create_file(const char *filename, char *text_content)
+int append_text_to_file(const char *filename, char *text_content)
 {
-	ssize_t fd, w = 0, len = 0;
+	ssize_t fd, w, len = 0;
 
 	if (!filename)
 		return (-1);
