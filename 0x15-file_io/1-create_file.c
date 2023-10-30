db6e1da7c206e180527a07dadc5e46ec20aae1cf
essahatim <hatimessabagh@gmail.com>
2023-10-30 14:02:47 -0700

    add changes

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,14 +14,13 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	if (text_content != NULL)
 	{
-		for (;text_content[len];)
+		for (; text_content[len];)
 			len++;
 	}
 	fd = open(filename, O_WRONLY | O_CRAET | O_TRUNC, 0600);
-	if (fd == -1)
+	w = write(fd, text_content, len);
+	if (fd == -1 || w == -1)
 		return (-1);
-	if (len)
-		w = write(fd, text_content, len);
-	close (fd);
-	return (w == len ? 1 : -1)
+	close(fd);
+	return (1);
 }
