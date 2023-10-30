b00073aeb6b529715c6811fa385b4d5bb2bca630
essahatim <hatimessabagh@gmail.com>
2023-10-30 11:57:34 -0700

    add changes

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -14,6 +14,8 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	ssize_t w, r;
 	char buf[1024 * 8];
 
+	if (!filename || !letters)
+		return (0);
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
