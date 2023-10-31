ff0dfc74b029f6769d9be1bb56fe226c25259bc0
essahatim <hatimessabagh@gmail.com>
2023-10-31 08:50:59 -0700

    add changes

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -24,24 +24,26 @@ void close_file(int fd)
 int main(int ac, char **av)
 {
 	char buf[1024];
-	ssize_t from, to, w, r;
+	int from = 0, to = 0;
+	ssize_t r, w;
 
 	if (ac != 3)
 		dprintf(STDERR_FILENO, USAGE), exit(97);
 	from = open(av[1], O_RDONLY);
-	if (from == -1)
-		dprintf(STDERR_FILENO, ER_NORD, av[1]), exit(98);
-	to = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, PERMISSION);
-	if (to == -1)
-		dprintf(STDERR_FILENO, ER_NOWR, av[2]), exit(99);
+	to = open(av[2],  O_WRONLY | O_CREAT | O_TRUNC, PERMISSION);
 	r = read(from, buf, 1024);
-	w = write(to, buf, r);
 	while (r > 0)
-		if (w != r)
+	{
+		if (from == -1 || r == -1)
+			dprintf(STDERR_FILENO, ER_NORD, av[1]), exit(98);
+
+		w = write(to, buf, r);
+		if (to == -1 || w == -1)
 			dprintf(STDERR_FILENO, ER_NOWR, av[2]), exit(99);
-	if (r == -1)
-		dprintf(STDERR_FILENO, ER_NORD, av[1]), exit(98);
+		to = open(av[2], O_WRONLY | O_APPEND);
+	}
+
 	close_file(from);
 	close_file(to);
-	return (0);
+	return (1);
 }
