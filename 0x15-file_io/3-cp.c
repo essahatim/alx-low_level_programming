8df15fd736b493433e067db3809b8675b9ed0119
essahatim <hatimessabagh@gmail.com>
2023-11-02 13:44:26 -0700

    changes

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,39 +1,47 @@
 #include "main.h"
+/**
+ * close_file - Closes the file.
+ * @fd: the file to be closed.
+ */
+void close_file(int fd)
+{
+	int cl;
 
+	cl = close(fd);
+	if (cl == -1)
+	{
+		dprintf(STDERR_FILENO, ER_NOCL, fd);
+		exit(100);
+	}
+}
 /**
  * main - Function program.
  * @ac: The argument of count.
  * @av: The argument of vector.
  *
- * Return: 1 (success) or 0 (failure)
+ * Return: 1 (success) or 0 (failure).
  */
 int main(int ac, char **av)
 {
 	char buf[1024];
-	int from = 0, to = 0;
-	ssize_t r, w;
+	ssize_t from, to, w, r;
 
 	if (ac != 3)
 		dprintf(STDERR_FILENO, USAGE), exit(97);
 	from = open(av[1], O_RDONLY);
-	to = open(av[2],  O_WRONLY | O_CREAT | O_TRUNC, PERMISSION);
-	r = read(from, buf, 1024);
-
-	if (from == -1 || r == -1)
+	if (from == -1)
 		dprintf(STDERR_FILENO, ER_NORD, av[1]), exit(98);
-	do
-	{
-		w = write(to, buf, r);
-		if (to == -1 || w == -1 || w != r)
+	to = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, PERMISSION);
+	if (to == -1)
+		dprintf(STDERR_FILENO, ER_NOWR, av[2]), exit(99);
+	r = read(from, buf, 1024);
+	w = write(to, buf, r);
+	while (r > 0)
+		if (w != r)
 			dprintf(STDERR_FILENO, ER_NOWR, av[2]), exit(99);
-		r = read(from, buf, 1024);
-		to = open(av[2], O_WRONLY | O_APPEND);
-	} while (r > 0);
-	from = close(from);
-	to = close(to);
-	if (from)
-		dprintf(STDERR_FILENO, ER_NOCL, from), exit(100);
-	if (to)
-		dprintf(STDERR_FILENO, ER_NOCL, from), exit(100);	
+	if (r == -1)
+		dprintf(STDERR_FILENO, ER_NORD, av[1]), exit(98);
+	close_file(from);
+	close_file(to);
 	return (1);
 }
